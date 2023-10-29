9828d5770485a3d24513cbd2ca72bb76f8956adc
marianna-trapotsi <54943411+marianna-trapotsi@users.noreply.github.com>
2023-10-29 20:40:27 +0000

    Update prepare_files.sh

--- Tools/prepare_files.sh
+++ Tools/prepare_files.sh
@@ -4,7 +4,6 @@
 #SBATCH --time=0-00:60:00
 
 
-#module load miniconda3-4.7.12.1-gcc-9.2.0-cg3qrnj
 conda activate gene_synteny
 python 2create_maps.py --species $1 --directory_read $2 --directory_write $3
 
